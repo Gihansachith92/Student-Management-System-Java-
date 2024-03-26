f29f1f0c9581390b4bd7161b756546520dcd0a8b
LoneWolf <imalkesara2001@gmail.com>
2024-03-26 18:24:01 +0530

    first commit

---
+++ Alldata.sql
@@ -0,0 +1,15 @@
+
+-- Admin table
+INSERT INTO "admin" (adminId,aName,phoneNumber,email,gender,pwd)
+VALUES ('admin001','admin',753383838,'admin@gmail.com','male','admin123');
+
+
+
+-- lecturer table
+INSERT INTO "admin" (lecturerId,lName,phoneNumber,email,gender,pwd,courceCode)
+VALUES  ('LEC001','Smith',1234567890,'smith@example.com','Male','pass123','ICT01'),
+        ('LEC002','Johnson',9876543210,'johnson@example.com','Female','abc@123','ICT02'),
+        ('LEC003','Brown',5555555555,'brown@example.com','Male','securePwd','ICT03'),
+        ('LEC004','Garcia',3333333333,'garcia@example.com','Female','p@ssw0rd','CS104'),
+
+
