ff18218e52d4dc6bcf188ad65c78f1dcaad7e6de
LoneWolf <imalkesara2001@gmail.com>
2024-04-12 12:41:36 +0530

    attendance

--- Alldata.sql
+++ Alldata.sql
@@ -8,47 +8,41 @@ INSERT INTO User (userId, type, uName, phoneNumber, email, gender, pwd, image) V
 ('TO001','TechnicalOfficer','John Smith',123456789,'john@example.com','Male','password1','technical.jpg'),
 ('TO002','TechnicalOfficer','Pushpa Doe',876543210,'jane@example.com','Female','password2','technical.jpg'),
 ('TG1001','Student','John Doe',123456789,'john@example.com','male','password1','john_doe.jpg'),
-('TG1002','Student','Jane Smith',987650321,'janeSmith@example.com','male','password2','jane_smith.jpg');
+('TG1002','Student','Jane Smith',987650321,'janeSmith@example.com','male','password2','jane_smith.jpg'),
+('TG1003','Student','David Johnson',555444333,'david@example.com','male','password3','jane_smith.jpg'),
+('TG1004','Student','Emily Davis',222333444,'emily@example.com','female','password4','jane_smith.jpg'),
+('TG1005','Student','Michael Brown',777888999,'michael@example.com','male','password5','michael_brown.jpg'),
+('TG1006','Student','Jennifer Wilson',333222111,'jennifer@example.com','female','password6','jennifer_wilson.jpg'),
+('TG1007','Student','Daniel Martinez',999888777,'daniel@example.com','male','password7','daniel_martinez.jpg'),
+('TG1008','Student','Jessica Taylor',666777888,'jessica@example.com','female','password8','jessica_taylor.jpg'),
+('TG1009','Student','Christopher Anderson',444555666,'christopher@example.com','male','password9','christopher_anderson.jpg'),
+('TG1010','Student','Amanda Thomas',111222333,'amanda@example.com','female','password10','amanda_thomas.jpg');
 
 
 
-
-
-
-
-
-
-
-
-
-
-
--- Admin table
-INSERT INTO "admin" (adminId,aName,phoneNumber,email,gender,pwd)
-VALUES ('admin001','admin',753383838,'admin@gmail.com','male','admin123');
-
-
-
--- lecturer table
-INSERT INTO "admin" (lecturerId,lName,phoneNumber,email,gender,pwd)
-VALUES  ('LEC001','Smith',1234567890,'smith@example.com','Male','pass123'),
-        ('LEC002','Johnson',9876543210,'johnson@example.com','Female','abc@123'),
-        ('LEC003','Brown',5555555555,'brown@example.com','Male','securePwd'),
-        ('LEC004','Garcia',3333333333,'garcia@example.com','Female','p@ssw0rd');
 
 -- Student table
 INSERT INTO Student (studentId, sName, phoneNumber, email, adress, img, gender, pwd) VALUES
-('TG1001', 'John Doe', 1234567890, 'john@example.com', '123 Main St, City, Country', 'john_doe.jpg', 'Male', 'password1'),
-('TG1002', 'Jane Smith', 9876543210, 'janeSmith@example.com', '456 Elm St, City, Country', 'jane_smith.jpg', 'Female', 'password2'),
-('TG1003', 'David Johnson', 5554443333, 'david@example.com', '789 Oak St, City, Country', 'david_johnson.jpg', 'Male', 'password3'),
-('TG1004', 'Emily Davis', 2223334444, 'emily@example.com', '101 Pine St, City, Country', 'emily_davis.jpg', 'Female', 'password4'),
-('TG1005', 'Michael Brown', 7778889999, 'michael@example.com', '202 Cedar St, City, Country', 'michael_brown.jpg', 'Male', 'password5'),
-('TG1006', 'Jennifer Wilson', 3332221111, 'jennifer@example.com', '303 Maple St, City, Country', 'jennifer_wilson.jpg', 'Female', 'password6'),
-('TG1007', 'Daniel Martinez', 9998887777, 'daniel@example.com', '404 Birch St, City, Country', 'daniel_martinez.jpg', 'Male', 'password7'),
-('TG1008', 'Jessica Taylor', 6667778888, 'jessica@example.com', '505 Walnut St, City, Country', 'jessica_taylor.jpg', 'Female', 'password8'),
-('TG1009', 'Christopher Anderson', 4445556666, 'christopher@example.com', '606 Spruce St, City, Country', 'christopher_anderson.jpg', 'Male', 'password9'),
-('TG1010', 'Amanda Thomas', 1112223333, 'amanda@example.com', '707 Birch St, City, Country', 'amanda_thomas.jpg', 'Female', 'password10');
+('TG1001', 'John Doe', 1234567890, 'john@example.com','john_doe.jpg', 'Male', 'password1'),
+('TG1002', 'Jane Smith', 9876543210, 'janeSmith@example.com','jane_smith.jpg', 'Female', 'password2'),
+('TG1003', 'David Johnson', 5554443333, 'david@example.com', 'david_johnson.jpg', 'Male', 'password3'),
+('TG1004', 'Emily Davis', 2223334444, 'emily@example.com','emily_davis.jpg', 'Female', 'password4'),
+('TG1005', 'Michael Brown', 7778889999, 'michael@example.com','michael_brown.jpg', 'Male', 'password5'),
 
+('TG1006', 'Jennifer Wilson', 3332221111, 'jennifer@example.com','jennifer_wilson.jpg', 'Female', 'password6'),
+('TG1007', 'Daniel Martinez', 9998887777, 'daniel@example.com','daniel_martinez.jpg', 'Male', 'password7'),
+('TG1008', 'Jessica Taylor', 6667778888, 'jessica@example.com','jessica_taylor.jpg', 'Female', 'password8'),
+('TG1009', 'Christopher Anderson', 4445556666, 'christopher@example.com','christopher_anderson.jpg', 'Male', 'password9'),
+('TG1010', 'Amanda Thomas', 1112223333, 'amanda@example.com','amanda_thomas.jpg', 'Female', 'password10');
+-- Admin table
+INSERT INTO "admin" (adminId,aName,phoneNumber,email,gender,pwd)
+VALUES ('admin001','admin',753383838,'admin@gmail.com','male','admin123');
+-- lecturer table
+INSERT INTO "admin" (lecturerId,lName,phoneNumber,email,gender,pwd)
+VALUES  ('LEC001','Smith',1234567890,'smith@example.com','Male','pass123'),
+('LEC002','Johnson',9876543210,'johnson@example.com','Female','abc@123'),
+('LEC003','Brown',5555555555,'brown@example.com','Male','securePwd'),
+('LEC004','Garcia',3333333333,'garcia@example.com','Female','p@ssw0rd');
 -- TechnicalOfficer table
 INSERT INTO TechnicalOfficer (tecId, tName, phoneNumber, email, gender, pwd) VALUES
 ('TO001', 'John Smith', 1234567890, 'john@example.com', 'Male', 'password1'),
@@ -64,19 +58,38 @@ INSERT INTO Cource (courceCode, cName, credit, weeek, theory, practical, userId)
 ('ICT05', 'E-Commerce Implementation, Management and Security', 3, 15, 1, 0, 'LEC004'),
 ('TCS2112', 'Business Economics', 3, 15, 9, 3, 'LEC004');
 
-INSERT INTO StudentMarks (q1, q2, q3, midP, midT, endP, endT, assesment, studentId, courceCode) VALUES
+
+
+CREATE TABLE StudentMarks(
+      q1 INT,
+      q2 INT,
+      q3 INT,
+      midP INT,
+      midT INT,
+      endP INT,
+      endT INT,
+      assesment INT,
+      userId VARCHAR(60),
+      courceCode VARCHAR(60),
+      FOREIGN KEY (userId) REFERENCES User(userId),
+      FOREIGN KEY (courceCode) REFERENCES Cource(courceCode)
+
+      
+);
+
+INSERT INTO StudentMarks (q1, q2, q3, midP, midT, endP, endT, assesment, userId, courceCode) VALUES
 
 -- Data Structures and Algorithms
 (80, 45, 56, 0, 56, 0, 78, 45, 'TG1001', 'ICT01'),
-(46, 12, 100, 0, 63, 0, 55, 66, 'TG1002', 'ICT01'),
+(46, 12, 100, 0, 63, 0, 55, 66,'TG1002', 'ICT01'),
 (34, 67, 33, 0, 12, 0, 65, 88, 'TG1003', 'ICT01'),
 (67, 56, 12, 0, 34, 0, 48, 55, 'TG1004', 'ICT01'),
-(67, 100, 12, 0, 12, 0, 45, 43, 'TG1005', 'ICT01'),
+(67, 100, 12, 0, 12, 0, 45, 43,'TG1005', 'ICT01'),
 (90, 78, 65, 0, 88, 0, 78, 34, 'TG1006', 'ICT01'),
 (45, 15, 45, 0, 34, 0, 35, 64, 'TG1007', 'ICT01'),
 (12, 45, 12, 0, 55, 0, 90, 34, 'TG1008', 'ICT01'),
 (03, 78, 04, 0, 10, 0, 10, 67, 'TG1009', 'ICT01'),
-(50, 36, 10, 0, 75, 0, 12, 49, 'TG1010', 'ICT01'),
+(50, 36, 10, 0, 75, 0, 12, 49, 'TG1010', 'ICT01');
 
 -- Object Oriented Programming
 (90, 92, 88, 0, 67, 0, 85, 90, 'TG1001', 'ICT03'),
@@ -140,8 +153,18 @@ INSERT INTO StudentMarks (q1, q2, q3, midP, midT, endP, endT, assesment, student
 
 
 
-INSERT INTO Attendance (studentId, courseCode, date, type, status)
-VALUES 
+
+CREATE TABLE attendance (
+    userID VARCHAR(60),
+    courseCode VARCHAR(60),
+    sessionDate DATE,
+    sessionType ENUM('Theory', 'Practical', 'Combined'),
+    A_status ENUM('Present', 'Absent'),
+    FOREIGN KEY (userID) REFERENCES User(userId),
+    FOREIGN KEY (courseCode) REFERENCES Cource(courceCode));
+
+
+INSERT INTO attendance (userID, courseCode, sessionDate, sessionType, A_status) VALUES 
 
     
     -- Monday /////////////////////////////////////////////////////////////////// Start
@@ -379,7 +402,7 @@ VALUES
 
    
     ("TG1003","ICT02","2024-01-03","Theory","Present"),
-    ("TG1003","ICT02","2024-01-10","Theory","Absent","MED001"),
+    ("TG1003","ICT02","2024-01-10","Theory","Absent"),
     ("TG1003","ICT02","2024-01-17","Theory","Present"),
     ("TG1003","ICT02","2024-01-24","Theory","Present"),
     ("TG1003","ICT02","2024-01-31","Theory","Present"),
@@ -527,7 +550,7 @@ VALUES
     ("TG1010","ICT02","2024-03-19","Theory","Present"),
     ("TG1010","ICT02","2024-03-27","Theory","Present"),
     ("TG1010","ICT02","2024-04-03","Theory","Present"),
-    ("TG1010","ICT02","2024-04-10","Theory","Present"),
+    ("TG1010","ICT02","2024-04-10","Theory","Present");
 
 
 
@@ -718,7 +741,7 @@ VALUES
     ("TG1010","ICT03","2024-03-18","Practical","Present"),
     ("TG1010","ICT03","2024-03-26","Practical","Present"),
     ("TG1010","ICT03","2024-04-02","Practical","Present"),
-    ("TG1010","ICT03","2024-04-09","Practical","Present"),
+    ("TG1010","ICT03","2024-04-09","Practical","Present");
 
     -- Tuesday /////////////////////////////////////////////////////////////////// End
 
@@ -913,7 +936,7 @@ VALUES
     ("TG1010","ICT04","2024-03-19","Theory","Absent"),
     ("TG1010","ICT04","2024-03-27","Theory","Absent"),
     ("TG1010","ICT04","2024-04-03","Theory","Present"),
-    ("TG1010","ICT04","2024-04-10","Theory","Present"),
+    ("TG1010","ICT04","2024-04-10","Theory","Present");
 
      --Wensday /////////////////////////////////////////////////////////////////// End
 
