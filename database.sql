05f9637a98c71fba8859b0c85044e5fbbbf9ba2f
Gihan_Sachith <gihansachith92@gmail.com>
2024-04-23 10:12:04 +0530

    change

--- database.sql
+++ database.sql
@@ -51,73 +51,6 @@ CREATE TABLE timeTable(
 
 
 
-
-
--- CREATE TABLE Admin(
---          adminId VARCHAR(20),
---          aName VARCHAR(20),
---          phoneNumber INT,
---          email VARCHAR(20),
---          gender VARCHAR(10),
---          pwd VARCHAR(8),
---          PRIMARY KEY(adminId)
-         
---          );
-
-
-
--- lecturer table
-
--- CREATE TABLE lecturer(
---          lecturerId VARCHAR(20),
---          lName VARCHAR(20),
---          phoneNumber INT,
---          email VARCHAR(20),
---          gender VARCHAR(10),
---          pwd VARCHAR(8),
---          PRIMARY KEY(lecturerId),
-         
---          );
-
-
--- student table
-
--- CREATE TABLE Student(
---          studentId VARCHAR(20),
---          sName VARCHAR(20),
---          phoneNumber INT,
---          email VARCHAR(20),
---          adress VARCHAR(30),
---          img VARCHAR(100),
---          gender VARCHAR(10),
---          pwd VARCHAR(8),
---          PRIMARY KEY(studentId)
-
-         
---          );
-
-
--- technical officer table
-
--- CREATE TABLE TechnicalOfficer(
---          tecId VARCHAR(20),
---          tName VARCHAR(20),
---          phoneNumber INT,
---          email VARCHAR(20),
---          gender VARCHAR(10),
---          pwd VARCHAR(8),
---          PRIMARY KEY(tecId)
-
-         
---          );
-
-
-
-
-
-
-
-
 -- student Marks table
 
 CREATE TABLE StudentMarks(
@@ -129,9 +62,9 @@ CREATE TABLE StudentMarks(
       endP INT,
       endT INT,
       assesment INT,
-      studentId VARCHAR(20),
+      userId VARCHAR(20),
       courceCode VARCHAR(20),
-      FOREIGN KEY (studentId) REFERENCES Student(studentId),
+      FOREIGN KEY(userId) REFERENCES user(userId),
       FOREIGN KEY (courceCode) REFERENCES Cource(courceCode)
 
       
