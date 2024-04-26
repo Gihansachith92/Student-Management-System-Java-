b7e305b0464934370fae7b4f0f8344c0eb5c4818
Gihan_Sachith <gihansachith92@gmail.com>
2024-04-26 14:21:57 +0530

    student grades

--- Views.sql
+++ Views.sql
@@ -45,3 +45,42 @@ a.courceCode = e.courceCode and
 a.courceCode = c.courceCode ;
 
 
+-- create the StudentGradePoint view
+create view StudentGradePoint AS 
+select  userId, courceCode, 
+   case
+      when(FinalMark>= 85 and FinalMark<= 100) then 'A+'
+      when(FinalMark>= 80 and FinalMark<= 84.999999) then 'A'
+      when(FinalMark>= 75 and FinalMark<= 79.999999) then 'A-'
+      when(FinalMark>= 70 and FinalMark<= 74.999999) then 'B+'
+      when(FinalMark>= 65 and FinalMark<= 69.999999) then 'B'
+      when(FinalMark>= 60 and FinalMark<= 64.999999) then 'B-'
+      when(FinalMark>= 55 and FinalMark<= 59.999999) then 'C+'
+      when(FinalMark>= 50 and FinalMark<= 54.999999) then 'C'
+      when(FinalMark>= 45 and FinalMark<= 49.999999) then 'C-'
+      when(FinalMark>= 40 and FinalMark<= 44.999999) then 'D+'
+      when(FinalMark>= 35 and FinalMark<= 39.999999) then 'D'
+      when(FinalMark>= 0 and FinalMark<= 34.999999) then 'F'
+      ELSE 'Not_Valid'
+   end   AS 'Grade' ,
+
+  case
+      when(FinalMark>= 85 and FinalMark<= 100) then 4.0
+      when(FinalMark>= 80 and FinalMark<= 84.999999) then 4.0
+      when(FinalMark>= 75 and FinalMark<= 79.999999) then 3.7
+      when(FinalMark>= 70 and FinalMark<= 74.999999) then 3.3
+      when(FinalMark>= 65 and FinalMark<= 69.999999) then 3.0
+      when(FinalMark>= 60 and FinalMark<= 64.999999) then 2.7
+      when(FinalMark>= 55 and FinalMark<= 59.999999) then 2.3
+      when(FinalMark>= 50 and FinalMark<= 54.999999) then 2.0
+      when(FinalMark>= 45 and FinalMark<= 49.999999) then 1.7
+      when(FinalMark>= 40 and FinalMark<= 44.999999) then 1.3
+      when(FinalMark>= 35 and FinalMark<= 39.999999) then 1.0
+      when(FinalMark>= 0 and FinalMark<= 34.999999) then 0
+      ELSE 'Not_Valid'
+   end   AS 'Grade_Point'
+
+from StudentFinal_Marks;
+
+
+
